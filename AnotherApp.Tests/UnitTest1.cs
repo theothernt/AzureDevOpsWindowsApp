6a5e01813e84eecab69763c02a4cfb58b3acdf7d
Neil Turner <neil@neilturner.com>
2019-05-22 21:22:33 +0100

    Added simple test that should fail

--- AnotherApp.Tests/UnitTest1.cs
+++ AnotherApp.Tests/UnitTest1.cs
@@ -1,14 +1,25 @@
-﻿using System;
+﻿using AnotherApp.WPF;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AnotherApp.Tests
 {
+    // https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-mstest
     [TestClass]
-    public class UnitTest1
+    public class PrimeServiceTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private readonly PrimeService _primeService;
+
+        public PrimeServiceTests()
         {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 should not be prime");
         }
     }
 }
---
+++ AnotherApp.WPF/PrimeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AnotherApp.WPF
+{
+    // https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-mstest
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Please create a test first");
+        }
+    }
+}
